da7c182bd87d695605208d33a6ee98254baad746
Kristi Nikolla <knikolla@bu.edu>
2015-04-06 17:43:33 -0400

    Added circle enemies

--- game.js
+++ game.js
@@ -1,38 +1,105 @@
+// Global Variables
 var stage;
-var circle;
+var player;
+var enemies = [];
 var keys = {};
+var enemySpeedMultiplier = 2;
+var enemyDirection = 1;
 
-var KEYCODE_LEFT = 37, 
-	KEYCODE_RIGHT = 39,
-	KEYCODE_UP = 38, 
-	KEYCODE_DOWN = 40;
+// Constants
+var SCREEN_X = 480;
+var SCREEN_Y = 600;
+var PLAYER_SIZE = 32;
+var PLAYER_HALFSIZE = PLAYER_SIZE / 2;
 
-function Init() {
+var ENEMY_ROWS = 2;
+var ENEMY_COLUMNS = 5;
+var ENEMY_SPACING = 64;
+var ENEMY_SPEED = 2;
+var ENEMY_SIZE = 32;
+var ENEMY_HALFSIZE = ENEMY_SIZE / 2;
+
+var KEY_LEFT = 37;
+var KEY_RIGHT = 39;
+var KEY_UP = 38;
+var KEY_DOWN = 40;
+
+function init() {
+	initStage();
+	initPlayer();
+	initEnemies();
+}
+
+function initStage() {
 	stage = new createjs.Stage("gameCanvas")
-	circle = new createjs.Shape();
-
-	circle.graphics.beginFill("red").drawCircle(0, 0, 50);
-	circle.x = 100;
-	circle.y = 100;
-	stage.addChild(circle);
-
 	createjs.Ticker.addEventListener("tick", tick);
-	
 	this.document.onkeydown = keyDown;
-    this.document.onkeyup = keyUp;
+	this.document.onkeyup = keyUp;
+}
+
+function initPlayer() {
+	player = new createjs.Shape();
+	player.graphics.beginFill("green").drawCircle(0, 0, PLAYER_HALFSIZE);
+	player.x = SCREEN_X / 2 - PLAYER_SIZE;
+	player.y = SCREEN_Y - PLAYER_SIZE - 32;
+	stage.addChild(player);
+}
+
+function initEnemies() {
+	var i, x;
+	for (i = 0; i <= ENEMY_ROWS; i++) {
+		for (x = 0; x <= ENEMY_COLUMNS; x++) {
+			var enemy = new createjs.Shape();
+			enemy.graphics.beginFill("blue").drawCircle(0, 0, 16);
+			enemy.x = ENEMY_SPACING * (x + 1);
+			enemy.y = ENEMY_SPACING * (i + 1);
+			stage.addChild(enemy);
+			enemies.push(enemy);
+		}
+	}
 }
 
 function tick(updateEvent) {
+	// convert from milliseconds to seconds
 	var delta = updateEvent.delta / 1000;
 
-	handleInput(updateEvent)
-	//circle.x = circle.x + (updateEvent.delta * 1);
+	handleInput(delta);
+	enemyUpdate(delta);
+	handleCollisions(delta);
+
 	stage.update();
 }
 
-function handleInput(updateEvent) {
-	if (keys[KEYCODE_LEFT]) 	circle.x -= 10;
-    if (keys[KEYCODE_RIGHT]) 	circle.x += 10;	
+function handleInput(delta) {
+	if (keys[KEY_LEFT]) 	player.x -= 10;
+	if (keys[KEY_RIGHT]) 	player.x += 10;
+}
+
+function handleCollisions(delta) {
+	playerCheckScreenBoundaries();
+	enemyCheckScreenBoundaries();
+}
+
+function enemyUpdate(delta) {
+	for (var i = 0; i < enemies.length; i++) {
+		enemies[i].x += ENEMY_SPEED * enemyDirection * enemySpeedMultiplier;
+	}
+}
+
+function playerCheckScreenBoundaries() {
+	if (player.x > SCREEN_X - PLAYER_HALFSIZE) {
+		player.x = SCREEN_X - PLAYER_HALFSIZE;
+	} else if (player.x < PLAYER_HALFSIZE) {
+		player.x = PLAYER_HALFSIZE;
+	}
+}
+
+function enemyCheckScreenBoundaries() {
+	for (var i = 0; i < enemies.length; i++) {
+		if (enemies[i].x > SCREEN_X - ENEMY_SIZE || enemies[i].x < ENEMY_SIZE) {
+			enemyDirection *= -1;
+		}
+	}
 }
 
 function keyDown(event) {
